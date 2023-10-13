8e4e4e6312b28d837a0e0fb0ee0310f07d8de5e2
jazz-min <achu.elite@gmail.com>
2023-10-13 09:09:17 -0700

    changes related to S3 connector,exceptions,constants and meta process

---
+++ xetra/common/meta_process.py
@@ -0,0 +1,16 @@
+"""
+Methods for processing the meta file
+
+"""
+
+class MetaProcess():
+
+    @staticmethod
+    def update_meta_file(self):
+        pass
+
+    @staticmethod
+    def return_date_list(self):
+        pass
+
+    
